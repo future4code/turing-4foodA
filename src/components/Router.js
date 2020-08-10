af024119738623608d99369d153441f8f39ae76c
Kamila Melo <kamilamelo5@hotmail.com>
2020-08-10 11:57:26 -0300

    Criação de rotas

---
+++ src/components/Router.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+
+function Router() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path = "/login">
+                    <LoginPage/>
+                </Route>
+                <Route exact path = "/sign-up">
+                    <SinupPage/>
+                </Route>
+                <Route exact path = "/address">
+                    <AddressPage/>
+                </Route>
+                <Route exact path = "/restaurant">
+                    <RestaurantPage/>
+                </Route>
+                <Route exact path = "/restaurant/details">
+                    <RestaurantDetailPage/>
+                </Route>
+                <Route exact path = "/profile-page">
+                    <ProfilePage/>
+                </Route>
+                <Route exact path = "/profile-page/edit/user">
+                    <ProfileEditPage/>
+                </Route>
+                <Route exact path = "/profile-page/edit/address">
+                    <ProfileAddressPage/>
+                </Route>
+                <Route exact path = "/car">
+                    <CarPage/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router 
