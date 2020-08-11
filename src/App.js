31105a3753bfc37bc74daed3a3a93d0a26e1c2fc
Kamila-Melo <65471288+Kamila-Melo@users.noreply.github.com>
2020-08-11 10:06:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/Footer';
-import SingupPage from './components/SignupPage'
+import Router from "./components/Router";
 
 function App() {
   return (
-    <div className="App">
-      <SignupPage />
-    </div>
+    <Router />
   );
 }
-
 export default App;
+    
