f156d59eea06da7a9c0cf247f64049c032c560ea
laisflaviosilva <laisflavio.silva@gmail.com>
2020-08-11 12:09:31 -0300

    Tela carrinho Vazio

--- src/components/CarPage/index.js
+++ src/components/CarPage/index.js
@@ -1,35 +1,54 @@
 import React, {useState} from 'react'
-
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 const CarPage = () => {
 
-  const [ endereco, setEndereco ] = useState('')
+  const [ endereco, setEndereco ] = useState('Rua retiro dos artistas')
   const [ frete, setFrete ] = useState('')
   const [ aPagar, setAPagar ] = useState('')
   const [ formaDePagamento, setFormaPagamento ] = useState('')
-  return <div>
+  const [value, setValue] = React.useState('female');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+
+
+  return <div className='carrinho'>
     <header className='Bar'>
       <h1 className='title'>Meu carrinho</h1>
     </header>
     <main>
       <div className='Rectangle'>
-        <p className='Endereco-de-etrega'>Endereço de entrega</p>
-        <p>{endereco}</p>
+        <p className='Endereco-de-entrega'>Endereço de entrega</p>
+        <p className='endereco'>{endereco}</p>
       </div>
-      <div>
+      <div className='text-car'>
         <h2 className='Text'>Carrinho Vazio</h2>
-        <p className='Frete'>Frete {frete}</p>
+        </div>
+        <div>
+        <p className='Frete'>Frete R$0,00{frete}</p>
+        <div className ='total'>
         <h3 className='SUBTOTAL'>SUBTOTAL</h3>
-        <p className='R0000'>R$ {aPagar}</p>
+        <p className='R0000'>R$0,00 {aPagar}</p>
+        </div>
       </div>
       <form className='pagamento'>
-        <h3>Forma de pagamento</h3>
-        <input type="radio" id="din" name="din" value="dinheiro" />
-        <label for="din">Dinheiro</label>
-        <input type="radio" id="din" name="din" value="dinheiro" />
-        <label for="din">Cartão de crédito</label>
+          <h3 className='forma-pagamento'>Forma de pagamento</h3>
+          <hr className='linha' />
+          <FormControl component="fieldset" className='formulario'>
+            <RadioGroup aria-label="" name="" value={value} onChange={handleChange}>
+              <FormControlLabel value="dinheiro" control={<Radio />} label="Dinheiro" />
+              <FormControlLabel value="cartao-credito" control={<Radio />} label="Cartão de Crédito" />
+            </RadioGroup>
+          </FormControl>
+        <button className='confirm' onClick={''}>Confirmar</button>
       </form>
-      <button className='confirm'>Confirmar</button>
     </main>
   </div>
 }
--- src/components/CarPage/styles.js
+++ src/components/CarPage/styles.js
@@ -1,21 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
   .Bar {
-    width: 22.5rem;
     height: 4rem;
     -webkit-backdrop-filter: blur(10px);
     backdrop-filter: blur(10px);
     box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
     background-color: #ffffff;
-      .Mobile-Signal {
-      width: 1.031rem;
-      height: 0.625rem;
-      border-radius: 1px;
-      background-color: var(--black);
-    }
     .Carrier {
-    width: 2.5rem;
     height: 0.875rem;
     font-family: SFProText;
     font-size: 0.75rem;
@@ -27,30 +20,44 @@ const GlobalStyle = createGlobalStyle`
     color: #030303;
     }
     .title {
-    width: 5.688rem;
-    height: 1.188rem;
-    font-family: 'Roboto', sans-serif;
+      height: 1.188rem;
+      font-family: Roboto;
+      font-size: 1rem;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: -0.39px;
+      text-align: center;
+      color: black;
+      margin: 0 auto;
+      padding: 1.95rem 5.75rem 0 5.813rem;
+  }
+}
+
+main {
+  .Rectangle {
+  height: 4.75rem;
+  background-color: #eeeeee;
+  }
+  .Endereco-de-entrega {
+    width: 20.5rem;
+    height: 1.125rem;
+    font-family: Roboto;
     font-size: 1rem;
     font-weight: normal;
     font-stretch: normal;
     font-style: normal;
     line-height: normal;
     letter-spacing: -0.39px;
-    text-align: center;
-    color: var(--black);
-    margin: 0 auto;
-  }
+    color: #b8b8b8;
+    display: flex;
+    justify-content: flex-start;
+    margin: 1rem;
+    padding-top: 1rem;
 }
-
-main {
-  .Rectangle {
-  width: 22.5rem;
-  height: 4.75rem;
-  background-color: #eeeeee;
-  }
-  .Endereco-de-entrega {
+.endereco {
   width: 20.5rem;
-  height: 1.125rem;
   font-family: Roboto;
   font-size: 1rem;
   font-weight: normal;
@@ -58,7 +65,14 @@ main {
   font-style: normal;
   line-height: normal;
   letter-spacing: -0.39px;
-  color: #b8b8b8;
+  color: black;
+  display: flex;
+  justify-content: flex-start;
+  padding: 0 1rem 0 1rem;
+}
+.text-car {
+  width: 22.5rem;
+  height: 2.625rem;
 }
 .Text {
   width: 18.5rem;
@@ -72,8 +86,9 @@ main {
   line-height: normal;
   letter-spacing: -0.39px;
   text-align: center;
-  color: var(--black);
+  color: black;
   margin: 0 auto;
+  margin: 0.75rem 2rem;
 }
 .Frete {
   width: 6.5rem;
@@ -86,7 +101,12 @@ main {
   line-height: normal;
   letter-spacing: -0.39px;
   text-align: right;
-  color: var(--black);
+  color: black;
+  padding: 2rem 1.125rem 0 15rem;
+  margin: 0;
+}
+.total {
+  display: flex;
 }
 .SUBTOTAL {
   width: 10.25rem;
@@ -98,7 +118,10 @@ main {
   font-style: normal;
   line-height: normal;
   letter-spacing: -0.39px;
-  color: var(--black);
+  color: black;
+  display: flex;
+  justify-content: flex-start;
+  margin-left: 1rem;
 }
 .R0000 {
   width: 10.25rem;
@@ -111,11 +134,18 @@ main {
   line-height: normal;
   letter-spacing: -0.43px;
   text-align: right;
-  color: var(--lipstick);
+  color: #e8222e;
+  padding-right: 1rem;
 }
-.confirm {
-  width: 20.5rem;
-  height: 2.625rem;
+}
+
+.pagamento {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-left: 1rem;
+  .forma-pagamento {
+  height: 1.125rem;
   font-family: Roboto;
   font-size: 1rem;
   font-weight: normal;
@@ -123,13 +153,28 @@ main {
   font-style: normal;
   line-height: normal;
   letter-spacing: -0.39px;
-  text-align: center;
-  color: var(--black);
-  border-radius: 2px;
-  background-color: rgba(232, 34, 46, 0.5);
-  border: none;
+  color: black;
+  display: flex;
+  justify-content: flex-start;
+  margin: 0;
+  padding-top: 1.5rem;
 }
+.linha {
+  width: 20.5rem;
+  background: #030303;
+  margin-right: 2rem;
+
+  }
+  .confirm {
+    height: 2.625rem;
+    border-radius: 2px;
+    background-color: rgba(232, 34, 46, 0.5);
+    border: none;
+    margin-right: 1.5rem;
+    margin-top: 4.063rem;
+  }
 }
+
 `
 
 
